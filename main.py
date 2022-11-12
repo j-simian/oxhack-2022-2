e60e47ce2601f4185985adc23ee9318485f23d92
Jake Simian <jake.simian@keble.ox.ac.uk>
2022-11-12 12:09:23 +0000

    window

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 
+(width, height) = (800, 600)
 
+pygame.display.set_caption("minimal program")
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+
+def game_loop():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+game_loop()
