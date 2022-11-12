eeb5d8fde3a95a5ee46a838debe8b87e859f1b73
Slush Puppy <46222402+Slush0Puppy@users.noreply.github.com>
2022-11-12 13:45:16 +0000

    Update levelloader.py

--- levelloader.py
+++ levelloader.py
@@ -2,4 +2,5 @@ from PIL import Image #pip install pillow
 import numpy as np
 image = Image.open("assets/level1/level.png")
 image_array = np.array(image)
-print(image_array)
+for each in image_array:
+    print(each)
