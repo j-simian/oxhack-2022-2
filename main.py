92147dc71c3c704148558f71ad4897f5e3e72c4e
Jake Simian <jake.simian@keble.ox.ac.uk>
2022-11-12 12:25:21 +0000

    player and gravity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import player
 
 (width, height) = (800, 600)
 
@@ -7,10 +8,14 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
 
+player = player.Player(100, 100)
+
 def game_loop():
     running = True
     while running:
         pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, width, height))
+        player.render(screen)
+        player.tick()
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
