f7612485f35df06e83404d278a8df9b68c541cd2
Slush Puppy <46222402+Slush0Puppy@users.noreply.github.com>
2022-11-12 15:59:17 +0000

    Update levelloader.py

--- levelloader.py
+++ levelloader.py
@@ -12,25 +12,31 @@ blocks = {
 class Level_Loader:
     def __init__(self, index):
         self.level_image = Image.open("assets/lvl_data/lvl"+str(index)+".png").convert("RGBA")
-        self.level_bounding_box = Image.open("assets/lvl_data/lvl"+str(index)+"b.png").convert("RGBA")
-    
-    def load_level(self,index):
-        level=[]
+        self.startlocation = (-1,-1)
+
+        self.level=[]
         for y in range(0, 27):
             level_row=[]
             for x in range(0,48):
                 r,g,b,a=(self.level_image.getpixel((x, y)))
                 hex_code = hex(r*16**6+g*16**4+b*16**2+a)[2:].zfill(8)
-                level_row+=[blocks[hex_code]]
+                block=blocks[hex_code]
+                if block==2:
+                    self.startlocation = (x*40,y*40)
+                level_row+=[block]
             level+=[level_row]
-        return(level)
+
+    
+    def load_level(self,index):
+        return(self.level)
 
     def load_bounding_box(self, index):
+        level_bounding_box = Image.open("assets/lvl_data/lvl"+str(index)+"b.png").convert("RGBA")
         level=[]
         for y in range(0, 27):
             level_row=[]
             for x in range(0,48):
-                r,g,b,a=(self.level_bounding_box.getpixel((x, y)))
+                r,g,b,a=(level_bounding_box.getpixel((x, y)))
                 if a==0:
                     level_row+=[0]
                 else:
@@ -39,4 +45,4 @@ class Level_Loader:
         return(level)
 
     def player_initial_position(self, index):
-        return
+        return self.startlocation
