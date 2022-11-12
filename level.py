34832e7bcfad55b2d24bfd399035201d02e3547e
Jake Simian <jake.simian@keble.ox.ac.uk>
2022-11-12 19:20:56 +0000

    ?

--- level.py
+++ level.py
@@ -1,21 +1,31 @@
 from levelloader import Level_Loader 
 import pygame
+import math
 
 class Level:
     def __init__(self, index):
         self.loader = Level_Loader(1)
         self.blocks = self.loader.load_level(1)
-        self.bg_img = pygame.image.load("./assets/art/background_stars.png").convert_alpha()
+        self.bg1 = pygame.image.load("./assets/art/background1.png").convert_alpha()
+        self.bg2 = pygame.image.load("./assets/art/background2.png").convert_alpha()
+        self.bg3 = pygame.image.load("./assets/art/background3.png").convert_alpha()
         self.radcam = pygame.image.load("./assets/art/background_radcam.png").convert_alpha()
-        self.level = pygame.transform.scale(pygame.image.load("./assets/lvl_data/lvl1.png").convert_alpha(), (1920, 1080))
+        self.fg = pygame.image.load("./assets/art/lvl1/foreground.png").convert_alpha()
+        self.mg = pygame.image.load("./assets/art/lvl1/midground.png").convert_alpha()
 
-    def draw_bg(self, screen):
-        screen.blit(self.bg_img, (0, 0))
-        screen.blit(self.radcam, (0, 20))
-        screen.blit(self.level, (0, 0))
+    def draw_bg(self, screen, frame):
+        bg_img = self.bg1 if (frame // 20) % 3 == 0 else self.bg2 if (frame // 20) % 3 == 1 else self.bg3
+        screen.blit(bg_img, (0, 0))
+        screen.blit(self.radcam, (0, math.sin(frame / 40)*15))
         # for i in range(0, len(self.blocks)):
         #     for j in range(0, len(self.blocks[i])):
         #         pygame.draw.rect(screen, (255, 0, 255) if self.blocks[i][j] == 1 else (255, 255, 255), pygame.Rect(i*40, j*40, 40, 40), 1)
+
+    def draw_fg(self, screen):
+        screen.blit(self.fg, (0, 0))
+
+    def draw_mg(self, screen):
+        screen.blit(self.mg, (0, 0))
 
     @staticmethod
     def pixel_to_tile(x, y):
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import game_objects
 from PIL import Image
 from level import Level
 
-(width, height) = (1920, 1080)
+(width, height) = (120, 1080)
 
 pygame.display.set_caption("oxsplat")
 pygame.init()
@@ -18,22 +18,26 @@ level = Level(1)
 clock = pygame.time.Clock() 
 
 def game_loop():
+    frame = 0
     running = 1
     while running:
         keys=pygame.key.get_pressed()
 
         # render
         pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, 0, width, height))
-        level.draw_bg(screen)
+        level.draw_bg(screen, frame)
         for i in objects:
             i.tick(level.blocks, keys)
         for i in objects:
             i.render(screen)
+        level.draw_mg(screen)
+        level.draw_fg(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = 0
         clock.tick(60)
+        frame += 1
 
 
 
