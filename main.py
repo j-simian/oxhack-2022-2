02db35eb249e47c913f9ad0d7d384e8bdffa7d62
Max Northcott <maxjnorthcott@hotmail.co.uk>
2022-11-13 07:31:05 +0000

    arifgjan

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import game_objects
 import microbit
 from PIL import Image
 from level import Level, levelWon, dead
+from random import randint
 
 (width, height) = (1920, 1080)
 
@@ -13,7 +14,7 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
 
 
-levelnumber = 12
+levelnumber = 1
 objects = []
 keys = []
 bit_keys = []
@@ -24,6 +25,8 @@ for each in level.boxes:
     objects+=[game_objects.Controllable_Box(each[0],each[1],each[2],each[3], levelnumber)]
 clock = pygame.time.Clock()
 start = False
+
+
 
 ROLL = 512
 PITCH = 513
@@ -37,6 +40,10 @@ def game_loop():
     running = 1
     roll, pitch = 0, 0
     alpha = 0
+    death1 = pygame.image.load("./assets/startscreens/death1.png").convert_alpha()
+    death2 = pygame.image.load("./assets/startscreens/death2.png").convert_alpha()
+    death3 = pygame.image.load("./assets/startscreens/death3.png").convert_alpha()
+    death4 = pygame.image.load("./assets/startscreens/death4.png").convert_alpha()
     while running:
 
         while not (levelWon or dead):   
@@ -47,8 +54,8 @@ def game_loop():
             roll, pitch, a, b = microbit.bitman(roll, pitch)
             bit_keys=(roll, pitch, a, b)
 
-            image = level.level_menu
             if not start:
+                image = level.level_menu
                 if alpha < 253: alpha += 2
                 screen.fill((0,0,0))    
                 image.set_alpha(alpha)    
@@ -74,6 +81,12 @@ def game_loop():
                         continue
                     elif state == 2:
                         dead = True
+                        alpha = 255
+                        rand = randint(1,100)
+                        if rand <= 33: image = death1
+                        elif rand <= 66: image = death2
+                        elif rand <= 99: image = death3
+                        else: image = death4
                         continue
                 if levelWon or dead:
                     continue
@@ -93,11 +106,13 @@ def game_loop():
             frame += 1
 
         
-        if levelWon: levelnumber += 1
-        level = Level(levelnumber)
+        if levelWon: 
+            levelnumber += 1
+            start = False
+            level = Level(levelnumber)
         player = game_objects.Player(level.player_position[0], level.player_position[1])
         objects = [player]
-        start, levelWon, dead = False, False, False
+        levelWon, dead = False, False
         for each in level.boxes:
             objects+=[game_objects.Controllable_Box(each[0],each[1],each[2],each[3], levelnumber)]
 
